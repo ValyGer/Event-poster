11938e4319a9a5dd375da11a4a0e9b6d14bceaec
Valentin <valentin.geras@yandex.ru>
2024-06-23 08:58:07 +0300

    Первоначальная настройка

---
+++ stats/src/main/java/ru/practicum/stat/Stats.java
@@ -0,0 +1,11 @@
+package ru.practicum.stat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stats {
+    public static void main(String[] args) {
+        SpringApplication.run(Stats.class, args);
+    }
+}
