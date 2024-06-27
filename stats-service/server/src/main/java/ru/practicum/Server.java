35a0e9f203bccfb1f32f6a3bbfa602de7042f74a
Valentin <valentin.geras@yandex.ru>
2024-06-28 01:58:50 +0300

    Борьба с неизвестной ошибкой при создании базы данных для работы установки

--- stats-service/server/src/main/java/ru/practicum/Server.java
+++ stats-service/server/src/main/java/ru/practicum/Server.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:application.properties")
+//@PropertySource("classpath:application.properties")
 public class Server {
     public static void main(String[] args) {
         SpringApplication.run(Server.class, args);
