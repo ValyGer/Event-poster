f931e3bbb8808aff38fd68139bd793d404a77de4
Valentin <valentin.geras@yandex.ru>
2024-07-20 15:06:00 +0300

    Исправление чекстайл.

--- ewm-service/src/main/java/ru/practicum/ewm/user/repository/UserRepository.java
+++ ewm-service/src/main/java/ru/practicum/ewm/user/repository/UserRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    List<User> findAllByIdIn(List<Long> usersId, Pageable Pageable);
+    List<User> findAllByIdIn(List<Long> usersId, Pageable pageable);
 }
 
 
